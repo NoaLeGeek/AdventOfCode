a8a58d2890c4e4f8eb8aee36f6d1327a03825683
NoaLeGeek <noacraft68@gmail.com>
2023-08-22 14:34:05 +0200

    Solved day-02 part 2

--- day-02/main.py
+++ day-02/main.py
@@ -1,18 +1,20 @@
+# part 1
 # A X --> Rock +1
 # B Y --> Paper +2
 # C Z --> Scissors +3
+# win +6 draw +3 lose +0
+# part 2
+# ABC stays the same
+# X Y Z --> +0 +3 +6
 
 def main():
     INPUT_FILE = "input-full.txt"
-    winning_pairs = ["A Y", "B Z", "C X"]
+    points = [[3, 1, 2], [1, 2, 3], [2, 3, 1]]
     score = 0
     with open(INPUT_FILE, "r") as file:
         data = file.read()
-        print(data.splitlines())
-        for pair in winning_pairs:
-            score += data.count(pair)*6
         for line in data.splitlines():
-            score += ord(line[-1])-87 + (3 if ord(line[0])+23==ord(line[-1]) else 0)
+            score += 3*(ord(line[-1])-88)+points[ord(line[0])-65][ord(line[-1])-88]
         print(score)
 
 
