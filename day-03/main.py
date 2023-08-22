da466afe5e8d0e5fad0b1ea52655ee8baa8f483f
NoaLeGeek <noacraft68@gmail.com>
2023-08-22 17:01:38 +0200

    Solved day-03 part 2

--- day-03/main.py
+++ day-03/main.py
@@ -2,7 +2,7 @@ def main():
     INPUT_FILE = "input-full.txt"
     with open(INPUT_FILE, "r") as file:
         data = file.read()
-        print(sum(map(lambda c: ord(c)-(38 if ord(c) < 91 else 96), map(lambda s: s.pop(), [set(filter(lambda x: x in line[:len(line)//2], line[len(line)//2:])) for line in data.splitlines()]))))
+        print(sum(map(lambda c: ord(c)-(38 if ord(c) < 91 else 96), map(lambda s: s.pop(), [set(group.splitlines()[0]) & set(group.splitlines()[1]) & set(group.splitlines()[2]) for group in ['\n'.join(data.split('\n')[i:i+3]) for i in range(0, len(data.split('\n')), 3)]]))))
 
 
 if __name__ == "__main__":
