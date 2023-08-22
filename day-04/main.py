2e3467c682b9f37766cb3eb13a49cad3add74209
NoaLeGeek <noacraft68@gmail.com>
2023-08-22 21:23:32 +0200

    Solved day-04 part 2

--- day-04/main.py
+++ day-04/main.py
@@ -9,7 +9,7 @@ def main():
         range1, range2 = lzt[i], lzt[i+1]
         if len(range1) > len(range2):
             range1, range2 = range2, range1
-        if range1.start >= range2.start and range1.stop <= range2.stop:
+        if range1.start < range2.stop and range2.start < range1.stop:
             assignment += 1
     print(assignment)
 
