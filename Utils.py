56707a09fece50dc9ad341caee68b59447759a42
NoaLeGeek <noacraft68@gmail.com>
2023-08-28 17:16:05 +0200

    Solved day-05 part 1

--- Utils.py
+++ Utils.py
@@ -2,5 +2,16 @@ from typing import AnyStr
 
 
 def read_input() -> AnyStr:
-    with open("input-test.txt" if input() == "test" else "input-full.txt", "r") as file:
+    with open("input-full.txt" if input() == "full" else "input-test.txt", "r") as file:
         return file.read()
+
+
+def replace_all(elements: list, old_segment: list, new_segment: list) -> list:
+    if len(old_segment) > len(elements):
+        return elements
+    if len(old_segment) == len(elements):
+        return elements if elements != old_segment else new_segment
+    for i in range(len(elements) - len(old_segment) + 1):
+        if elements[i:i + len(old_segment)] == old_segment:
+            elements[i:i + len(old_segment)] = new_segment
+    return elements
--- day-05/main.py
+++ day-05/main.py
@@ -1,10 +1,23 @@
+from copy import copy
+
 import Utils
+import re
 
 
 def main():
     data = Utils.read_input()
-
-
+    crates, moves = map(lambda x: list(str.splitlines(x)), data.split("\n\n"))
+    columns_length = int(crates[-1][-1])
+    crates = list(map(lambda x: Utils.replace_all(x.split(" "), ['', '', '', ''], ['']), crates[:-1]))
+    columns = [[crate[i] for crate in crates if i < len(crate) and crate[i] != ''] for i in range(columns_length)]
+    print(len(columns))
+    for move in moves:
+        n, move_from, move_to = map(int, re.findall(r"\d+", move))
+        move_from, move_to = move_from - 1, move_to - 1
+        items = list(filter(lambda x: x != '', columns[move_from]))[:n][::-1]
+        columns[move_to] = items + list(filter(lambda x: x != '', columns[move_to]))
+        columns[move_from] = columns[move_from][n:]
+    print("".join(map(lambda x: x[0], columns)).replace("[", "").replace("]", ""))
 
 if __name__ == "__main__":
     main()
