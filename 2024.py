791eed4a2e3826a45922d284e7be3db0cf3585e3
NoaLeGeek <noacraft68@gmail.com>
2024-12-01 11:59:03 +0100

    E

---
+++ 2024.py
@@ -0,0 +1,11 @@
+from  import Utils
+
+
+def main():
+    data = Utils.read_input()
+    print(data)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2024/day01/main.py
@@ -0,0 +1,12 @@
+import os
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "input-full.txt" if input() == "full" else "input-test.txt"), "r") as file:
+        data = file.read()
+
+def part1():
+    pass
+
+def part2():
+    pass
+
+print(data)
