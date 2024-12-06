f94c4fea566e4fc1a3a723459bc72f768a32622b
NoaLeGeek <noacraft68@gmail.com>
2024-12-06 18:52:46 +0100

    Advent of code Day 02 all parts

--- 2024/day01/main.py
+++ 2024/day01/main.py
@@ -1,19 +1,19 @@
 import os
 
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "input-full.txt" if input() == "full" else "input-test.txt"), "r") as file:
-    data = file.read()
+    input = file.read()
 
-def part1(data):
-    duos = [groups.split("   ") for groups in data.split("\n")]
+def part1(input):
+    duos = [groups.split("   ") for groups in input.split("\n")]
     lists = [sorted([int(duo[i]) for duo in duos]) for i in range(2)]
     distance = [abs(lists[0][i] - lists[1][i]) for i in range(len(lists[0]))]
     return sum(distance)
 
-def part2(data):
-    duos = [groups.split("   ") for groups in data.split("\n")]
+def part2(input):
+    duos = [groups.split("   ") for groups in input.split("\n")]
     lists = [[int(duo[i]) for duo in duos] for i in range(2)]
     score = sum([number * lists[1].count(number) for number in lists[0]])
     return score
 
-print(part1(data))
-print(part2(data))
+print(part1(input))
+print(part2(input))
