7599bc1ac834687fd1027b85fbb20e67f9dbd5cb
NoaLeGeek <noacraft68@gmail.com>
2025-06-26 21:02:28 +0200

    Solved day 2 2024

--- src/2024/day01/solution.py
+++ src/2024/day01/solution.py
@@ -2,18 +2,18 @@ from aoc.base import AoCProblem
 
 class Day01(AoCProblem):
     def parse(self, raw_input: str):
-        l = [[], []]
+        t = [[], []]
         for line in raw_input.splitlines():
             a, b = line.split()
-            l[0].append(a)
-            l[1].append(b)
-        return l
+            t[0].append(a)
+            t[1].append(b)
+        return t
 
     def part1(self):
-        l = [sorted(self.data[0]), sorted(self.data[1])]
+        t = [sorted(self.data[0]), sorted(self.data[1])]
         s = 0
-        for i in range(len(l[0])):
-            s += abs(int(l[0][i]) - int(l[1][i]))
+        for i in range(len(t[0])):
+            s += abs(int(t[0][i]) - int(t[1][i]))
         return s
 
     def part2(self):
--- src/2024/day02/solution.py
+++ src/2024/day02/solution.py
@@ -2,10 +2,34 @@ from aoc.base import AoCProblem
 
 class Day02(AoCProblem):
     def parse(self, raw_input: str):
-        pass
+        t = []
+        for line in raw_input.splitlines():
+            t.append(list(map(int, line.split())))
+        return t
+
+    def is_valid(self, row):
+        if sorted(row) not in (row, row[::-1]):
+            return False
+        for i in range(len(row) - 1):
+            if not (0 < abs(row[i] - row[i + 1]) < 4):
+                return False
+        return True 
 
     def part1(self):
-        pass
+        s = 0
+        for row in self.data:
+            if self.is_valid(row):
+                s += 1
+        return s
 
     def part2(self):
-        pass
+        s = 0
+        for row in self.data:
+            if self.is_valid(row):
+                s += 1
+            else:
+                for i in range(len(row)):
+                    if self.is_valid(row[:i] + row[i + 1:]):
+                        s += 1
+                        break
+        return s
