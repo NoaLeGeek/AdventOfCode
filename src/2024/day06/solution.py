8791d3d9bfbef9212259bb57f202d8d8fbc0387f
NoaLeGeek <noacraft68@gmail.com>
2025-07-02 15:09:33 +0200

    optimized day 06 2024

--- src/2024/day06/solution.py
+++ src/2024/day06/solution.py
@@ -4,14 +4,12 @@ from functools import cmp_to_key
 class Day06(AoCProblem):
     def parse(self, raw_input: str):
         index = raw_input.index("^")
-        lines = raw_input.splitlines()
-        return lines, index // (len(lines[0])+1), index % (len(lines[0])+1)
-
-    def part1(self):
-        grid, x, y = self.data
+        grid = raw_input.splitlines()
+        start = index // (len(grid[0]) + 1), index % (len(grid[0]) + 1)
+        x, y = start
         visited = set()
-        visited.add((x, y))
         dx, dy = (-1, 0)
+        visited.add((x, y, dx, dy))
         while 0 <= x + dx < len(grid) and 0 <= y + dy < len(grid[0]):
             nx, ny = x + dx, y + dy
             # Turn right 90 degrees
@@ -19,23 +17,34 @@ class Day06(AoCProblem):
                 dx, dy = dy, -dx
             else:
                 x, y = nx, ny
-                visited.add((x,y))
+                # Avoid infinite loops
+                if (x, y, dx, dy) in visited:
+                    break
+                visited.add((x,y,dx,dy))
+        return grid, start, visited
+
+    def part1(self):
+        visited = {(x, y) for x, y, _, _ in self.data[2]}
+        self.data = self.data[0], self.data[1], visited
         return len(visited)
     
     def part2(self):
-        grid, x, y = self.data
+        grid, start, visited = self.data
         s = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] != "#" and grid[i][j] != "^" and self.is_a_loop(grid, (x,y), (i,j)):
+        for i, j in visited:
+                # Skip the starting point
+                if (i, j) == start:
+                    continue
+                if self.is_a_loop(grid, start, (i, j)):
                     s += 1
         return s
 
-    def is_a_loop(self, grid, pos, obs):
-        x, y = pos
+    def is_a_loop(self, grid, start, obs):
+        x, y = start
         visited = set()
         dx, dy = (-1, 0)
         visited.add((x, y, dx, dy))
+        i = 0
         while 0 <= x + dx < len(grid) and 0 <= y + dy < len(grid[0]):
             nx, ny = x + dx, y + dy
             # Turn right 90 degrees
@@ -46,4 +55,5 @@ class Day06(AoCProblem):
                 if (x, y, dx, dy) in visited:
                     return True
                 visited.add((x,y,dx,dy))
+            i += 1
         return False
